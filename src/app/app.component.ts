b9f6ff55b0c1426811522394dd4bcfea0344881e
Brian_Sablich <sablichbrian@yahoo.com.ar>
2025-02-06 14:55:01 -0300

    Child component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { FormsModule } from '@angular/forms';
+import { ChildComponent } from "./components/child/child.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, FormsModule],
+  imports: [RouterOutlet, HeaderComponent, FormsModule, ChildComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -16,6 +17,7 @@ export class AppComponent {
   condicion: boolean = false;
   numero: number = 0;
   numerosArray: string[] = ['uno', 'dos', 'tres', 'cuatro', 'cinco'];
+  text: string = 'Soy la pantera flauta';
 
   animals: any = [
       {
---
+++ src/app/components/child/child.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-child',
+  imports: [],
+  templateUrl: './child.component.html',
+  styleUrl: './child.component.css'
+})
+export class ChildComponent {
+  @Input()
+  mensaje: String = "";
+
+  gato: any = {
+    "id": 1,
+    "name": "Tobias",
+    "age": 3
+  }
+
+}
