153f5950d4a2b1e70126f2bb158f2e5455ee7043
Brian_Sablich <sablichbrian@yahoo.com.ar>
2025-02-11 19:48:04 -0300

    Routers

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ListCatsComponent } from './pages/list-cats/list-cats.component';
+import { CreateComponent } from './pages/create/create.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: HomeComponent},
+    {path: 'list-cats', component: ListCatsComponent},
+    {path: 'create', component: CreateComponent}
+];
---
+++ src/app/pages/list-cats/list-cats.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-cats',
+  imports: [],
+  templateUrl: './list-cats.component.html',
+  styleUrl: './list-cats.component.css'
+})
+export class ListCatsComponent {
+
+}
